b1e2f76096f6d0a1fdb6f0bf5e9c48adfb8c4a85
Alaysha <alaysha_justice@yahoo.com>
2022-11-14 21:47:14 -0500

    alot

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //bring the express module here
+const { response } = require("express");
 let express = require ("express");
 
 // initiate the app
@@ -22,17 +23,49 @@ Return a summary of all the todo items
 
 
 
+app.get("/todos", function(){
+    res.json(db); //send whole database back
+})
+
 /*
 GET ToDo
 Return the details to a matching idea
 */
 
+app.get(*/todos/:id, function(){
+    let id = req.params.id; // id we are looking for
+    let matchingItem; // where we store 
+
+  
+  //loops thru positions
+    for(let i=0, i<db.length; i++) {
+        let entry = db[i];
+        if(entry.id == id){
+            matchingItem = entry;
+            break;
+        }
+    }
+
+    res.json(matchingItem);
+})
 
 /*DELETE ToDo with the item from the matching ID
 return OK message
 */
 
+app.delete("/todos/id", function (){
+    let id = req.params.id;
 
+
+    for(let i=0; i<db.length; i++){
+        if(id == db[i].id){
+            db.splice(i, 1);
+            break;
+        }
+    }
+
+    res.sendStatus(204);
+})
 
 
 /*
@@ -42,13 +75,13 @@ needs an input for task and description
 return an OK msg
 */
 
-app.post("/todos" function(){
+app.post("/todos", function(){
     let paylod = req.body;
 
    //get task infrom from the request body
     let task = payload.task;
     let description = payload.description;
-    
+
    //get an id we can use
     let id = nextId;
 
@@ -63,6 +96,11 @@ app.post("/todos" function(){
     }
 
     db.push(element);
+
+
+    // 400 your fault error. 500 computer fault error. 200 good. 
+    //200 good plus info avail. 204 good no info avail. 
+    res.sendStatus(204); //a thumbs up
 })
 
 /*PUT ToDo
